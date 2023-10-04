4b5ffd68b248268bd1922c85f3708c232e34ea80
blueberry <blueberry@ice.org>
2023-10-04 19:31:42 +0800

    更新规则脚本

--- source/rule/main.go
+++ source/rule/main.go
@@ -47,7 +47,7 @@ var client = resty.New().
 	SetTimeout(time.Minute * 10).
 	SetRetryWaitTime(time.Second * 30).
 	SetRetryCount(10).
-	SetProxy("http://192.168.1.8:7890").
+	//SetProxy("http://192.168.1.8:7890").
 	SetTLSClientConfig(&tls.Config{
 		InsecureSkipVerify: true,
 	})
