f85f62f4dd1fbe27506211438a3f4b88db60c9e5
blueberry <blueberry@ice.org>
2024-10-24 19:23:12 +0800

    update rule

--- source/rule/collector/client.go
+++ source/rule/collector/client.go
@@ -10,7 +10,7 @@ var client = resty.New().
 	SetTimeout(time.Minute * 10).
 	SetRetryWaitTime(time.Second * 30).
 	SetRetryCount(10).
-	//SetProxy("http://192.168.1.8:7890").
+	//SetProxy("http://127.0.0.1:7899").
 	SetTLSClientConfig(&tls.Config{
 		InsecureSkipVerify: true,
 	})
